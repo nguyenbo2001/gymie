6dc93a7fc8816a31c555d96cec45c6f0ee27ecb9
Bo Nguyen <nguyenbo2001@gmail.com>
2019-12-16 15:03:30 +0000

    make auth

---
+++ app/Http/Controllers/UsersController.php
@@ -0,0 +1,139 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use Lubus\Constants\Status;
+use Illuminate\Http\Request;
+
+class UsersController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $users = User::excludeArchive()->paginate(10);
+
+        return view('users.index', compact('users'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('users.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'email' => 'unique:users,email'
+        ]);
+
+        $user = [
+            'name' => $request->name,
+            'email' => $request->email,
+            'photo' => '',
+            'password' => bcrypt($request->password),
+            'status' => $request->status,
+        ];
+        $user = new User($user);
+        $user->save();
+
+        if ($user->id) {
+            $user->photo = \constFilePrefix::StaffPhoto.$user->id.'.jpg';
+            $user->save();
+            \Utilities::uploadFile($request, \constFilePrefix::StaffPhoto, $user->id, 'photo', \constPaths::StaffPhoto);
+
+            flash()->success('User was successfully registered');
+            return redirect('users');
+        } else {
+            flash()->error('Error while user registration');
+            return redirect('users');
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $user = User::findOrFail($id);
+
+        return view('users.show', compact('user'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $user = User::findOrFail($id);
+
+        return view('users.edit', compact('user'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $user = User::findOrFail($id);
+        $user->name = $request->name;
+        $user->email = $request->email;
+        if ($request->password != '') {
+            $user->password = bcrypt($request->password);
+        }
+        $user->status = $request->status;
+        $user->update();
+        $user->photo = \constFilePrefix::staffPhoto.$user->id.'.jpg';
+        $user->save();
+
+        \Utilities::uploadFile($request, \constFilePrefix::staffPhoto, $user->id, 'photo', \constPaths::staffPhoto);
+
+        flash()->success('User details was successfully updated');
+        return redirect('users');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+
+    public function archive(Request $request, $id) {
+        $user = User::findOrFail($id);
+        $user->status = \constStatus::Archive;
+        $user->save();
+
+        flash()->error('User was successfully deleted');
+        return redirect('users');
+    }
+}
